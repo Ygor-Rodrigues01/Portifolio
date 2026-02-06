e422277fe9ffac015ab7007f9a2094b1ce64c5f7
Ygor-Rodrigues01 <rodriguesygor2008@gmail.com>
2026-02-06 08:08:09 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/projetos')
+def projects():
+    return render_template('projetos.html')
+
+@app.route('/contatos')
+def contact():
+    return render_template('contatos.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
